d062e6e281568f760b8a19fccfdb0fb969b4b320
Dass Mk <kalidasm610@gmail.com>
2017-11-21 23:13:14 +0530

    Modal to display error message

---
+++ src/components/ErrorModal.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const ErrorModal = ({
+  message,
+  ...restProps,
+}) => (
+    <div className='modal' id='exampleModal' tabIndex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+      <div className='modal-dialog modal-sm modal-notify modal-danger' role='document'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title' id='exampleModalLabel'>{message}</h5>
+            <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+              <span aria-hidden='true'>&times;</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+ErrorModal.propTypes = {
+  message: PropTypes.string,
+};
+
+ErrorModal.defaultProps = {
+  message: 'Error',
+};
+
+
+
+export default ErrorModal;
