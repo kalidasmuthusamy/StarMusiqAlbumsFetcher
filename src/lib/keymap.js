b5cb01415945806419f54a06cb459ad25e066bbd
Dass Mk <kalidasm610@gmail.com>
2017-12-07 22:58:29 +0530

    Dynamic generation of mapped keyboard shortcuts

--- src/lib/keymap.js
+++ src/lib/keymap.js
@@ -1,6 +1,23 @@
-export default {
+import { range, map } from 'lodash';
+
+/* Number Should be at the end since Keyboard event will emit info only for last key pressed
+   An Album Link is selected from indexed links based on Number provided
+*/
+const albumCardsShortCuts = (keyCode = '', albumsCount) => (
+  map(range(albumsCount), (num) => (
+    `${keyCode}+${num}`
+  ))
+)
+
+const albumContainerShortcuts = {
   ALBUMS_CONTAINER: {
     GO_TO_PREVIOUS_PAGE: 'ctrl+left',
-    GO_TO_NEXT_PAGE: 'ctrl+right',
-  },
-}
+    GO_TO_NEXT_PAGE: ['ctrl+right'],
+    CLICK_ALBUM_STREAM_LINK: albumCardsShortCuts('shift', 10),
+    CLICK_ALBUM_IS_LINK: albumCardsShortCuts('ctrl', 10),
+    CLICK_ALBUM_ND_LINK: albumCardsShortCuts('meta', 10),
+    CLICK_ALBUM_HD_LINK: albumCardsShortCuts('alt', 10),
+  }
+};
+
+export default albumContainerShortcuts;
