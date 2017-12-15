18120f2aa6f2902630f052d5c7aa263193b876fe
Dass Mk <kalidasm610@gmail.com>
2017-12-15 20:42:10 +0530

    Deployment Setup

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath
+    publicPath: config.output.publicPath,
+    disableHostCheck: true,
   })
-  .listen(3000, '0.0.0.0', function (err, result) {
+  .listen(process.env.PORT || 3000, function (err, result) {
     if (err) {
       console.log(err);
     }
