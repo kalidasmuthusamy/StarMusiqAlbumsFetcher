244ae7f22376b08859161b1e5049c188e4646621
KalidasM <kalidasm610@gmail.com>
2018-05-05 20:51:38 +0530

    Allow Notification

--- sw-test/index.js
+++ sw-test/index.js
@@ -1,3 +1,23 @@
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function (reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function (sub) {
+        console.log('Endpoint URL: ', sub.endpoint);
+      }).catch(function (e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
+}
+
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw-test/sw.js').then(function (reg) {
 
@@ -9,10 +29,24 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker active');
     }
 
-    console.log('Scope: ', reg.scope);
+    reg.pushManager.getSubscription().then(function (sub) {
+      if (sub === null) {
+
+        Notification.requestPermission(function (status) {
+          console.log('Notification permission status:', status);
+          subscribeUser();
+        });
+
+        console.log('Not subscribed to push service!');
+      } else {
+        // We have a subscription, update the database
+        console.log('Subscription object: ', sub);
+      }
+    });
 
   }).catch(function (error) {
     // registration failed
     console.log('Registration failed with ' + error);
   });
+
 }
