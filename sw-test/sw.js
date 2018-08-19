35bbe8ffebd23fad10d5ce8113a487b1885dc615
KalidasM <kalidasm610@gmail.com>
2018-08-19 16:30:37 +0530

    Get Subscription Sw

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -1,5 +1,27 @@
 self.addEventListener('install', function (event) {
-  console.log(event);
+  // TODO: Callbacks after service worker installed
+});
 
-  console.log('Inside Install Callback');
+
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push StarMusiq';
+  const options = {
+    body: 'Yay it works, Dass!',
+    icon: null,
+    badge: null
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    // clients.openWindow('https://developers.google.com/web/')
+  );
 });
