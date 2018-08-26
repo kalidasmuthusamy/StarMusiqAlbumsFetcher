1cab8b5248f718501d73d22078da66d2018c0163
KalidasM <kalidasm610@gmail.com>
2018-08-26 19:33:32 +0530

    Webpack config for Dev and Prod

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'babel-polyfill',
+    // 'webpack-dev-server/client?http://localhost:3000',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new CompressionPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+  ]
+}
