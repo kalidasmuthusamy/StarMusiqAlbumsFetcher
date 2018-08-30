57719216b9e00be2120ddf13486fd2f9b58d1a60
KalidasM <kalidasm610@gmail.com>
2018-08-30 18:50:02 +0530

    Sample Endpoint to return stringified first page albums

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import path from 'path';
 import db from './db';
 import logger from './logger';
 
-import TestModel from './models/TestModel';
+import StarMusiqAlbumsFetcher from '../client/src/lib/CORSEnabledStarMusiqAlbumFetcher';
+import Album from './models/Album';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,11 +14,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client')));
   // Handle React routing, return all requests to React app
   app.get('*', function (_req, res) {
-    const newTestRecord = new TestModel();
-    newTestRecord.save();
     res.sendFile(path.join(__dirname, '../client', 'index.html'));
   });
 }
+
+app.get('/populate-new-album', (req, res) => {
+  const starMusiqAlbumsRetriever = new StarMusiqAlbumsFetcher();
+  const latestAlbumsPageNumber = 1;
+  const fetchedAlbums = starMusiqAlbumsRetriever.fetchAlbums(latestAlbumsPageNumber);
+
+  res.send(JSON.stringify(fetchedAlbums));
+});
 
 app.listen(port, () => {
   logger(`Listening on port ${port}`);
---
+++ server/models/Album.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  albumName: {
+    type: String,
+    required: true,
+  },
+  musicDirector: {
+    type: String,
+  },
+  casts: {
+    type: String,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  movieUrl: {
+    type: String,
+    required: true,
+  },
+  movieIcon: {
+    type: String,
+    required: true,
+  },
+  streamingUrl: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  }
+});
+
+// TODO:
+// Setters for download Links using Fetcher Module
+
+const AlbumModel = mongoose.model('AlbumModel', AlbumSchema)
+
+export default AlbumModel;
